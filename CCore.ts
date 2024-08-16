11dc1f9a4aa0101ac06c794be59f08f729fa15a9
Slendy4514 <52763557+Slendy4514@users.noreply.github.com>
2024-08-15 21:06:20 -0600

    Inicial

---
+++ CCore.ts
@@ -0,0 +1,9 @@
+import UIElement from "./UIElement";
+import dbAdapter from "./dbAdapter";
+import jsonDB from "./jsonDB";
+import Selector from "./Selector";
+
+export {UIElement}
+export {dbAdapter}
+export {jsonDB}
+export {Selector}
